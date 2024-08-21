304e6edbf37ce4d744ec6348a9578a5dc96f1e7c
Ganesh Patra <ganeshpatra550@gmail.com>
2024-08-21 11:53:41 +0530

    minor change

--- features/layout/layout.js
+++ features/layout/layout.js
@@ -1,4 +1,4 @@
-import { ImageBackground, SafeAreaView, StyleSheet, TextInput, View } from "react-native";
+import { ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
 import Login from "../login/login";
 import { StatusBar } from 'expo-status-bar';
 import { LinearGradient } from "expo-linear-gradient";
@@ -14,19 +14,7 @@ const Layout = ({ children }) => {
       return <AppLoading />;
     }
     return (
-        <LinearGradient colors={['yellow', 'green']} style = {styles.appContainer}>
-          <ImageBackground 
-            source={require('../../assets/images/leaf-background.jpg')} 
-            resizeMode={cover}
-            style = {styles.appContainer}
-            imageStyle={styles.backgroundImage}>
-              <StatusBar style='dark' />
-              <SafeAreaView style = {styles.appContainer}>
-                <Ionicons name="add-circle" size={24} color={white}/>
-                <Login />
-              </SafeAreaView>
-          </ImageBackground>
-        </LinearGradient>
+        <View></View>
     )
 }
 
@@ -34,7 +22,7 @@ const styles = StyleSheet.create({
     appContainer: {
       paddingTop: 50,
       paddingHorizontal: 60,
-      backgroundColor: '#e3e5d5',
+      // backgroundColor: '#e3e5d5',
       flex: 1,
       fontFamily: 'open-sans-bold'
     },

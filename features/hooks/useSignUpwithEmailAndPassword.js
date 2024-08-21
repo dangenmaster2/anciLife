e0250f4862a0506502443af14b2fb7c42b3197b4
Ganesh Patra <ganeshpatra550@gmail.com>
2024-08-21 12:41:03 +0530

    signup successfull

--- features/hooks/useSignUpwithEmailAndPassword.js
+++ features/hooks/useSignUpwithEmailAndPassword.js
@@ -2,11 +2,14 @@ import { createUserWithEmailAndPassword } from '@firebase/auth';
 import { auth } from '../../firebase/firebase';
 
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectUserEmail, selectUserPassword } from '../login/login.slice';
 
 const useSignUpwithEmailAndPassword = () => {
   const [isLoading, setIsLogin] = useState(false);
 
   const signUp = async (email, password) => {
+    console.log('email in auth', email, password)
     try {
       setIsLogin(true);
       await createUserWithEmailAndPassword(auth, email, password);
--- features/login/SignUp.js
+++ features/login/SignUp.js
@@ -1,19 +1,61 @@
-import { View, TextInput, Pressable, Text } from 'react-native';
+import { View, TextInput, Pressable, Text, StyleSheet } from 'react-native';
 
 import useSignUpwithEmailAndPassword from '../hooks/useSignUpwithEmailAndPassword';
+import { useState } from 'react';
+import { setGlobalPassword, setUserEmail } from './login.slice';
+import { useDispatch } from 'react-redux';
 
 const SignUp = () => {
   const { isLoading, signUp } = useSignUpwithEmailAndPassword();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleEmail = (text) => {
+    console.log('email ', text)
+    setEmail(text);
+    dispatch(setUserEmail(text))
+  }
+  const handlePassword = (text) => {
+    console.log('password ', text)
+    setPassword(text);
+    dispatch(setGlobalPassword(text))
+  }
+
   return (
-    <View>
-      <TextInput placeholder='Enter your email' />
-      <TextInput placeholder='Enter your password' />
-      <TextInput placeholder='Confirm Password' />
-      <Pressable>
+    <View style={styles.signUpContainer}>
+      <TextInput style={styles.inputContainer} onChangeText={handleEmail} 
+      placeholder='Enter your email' />
+      <TextInput style={styles.inputContainer} 
+      onChangeText={handlePassword}
+      placeholder='Enter your password' />
+      <TextInput style={styles.inputContainer} placeholder='Confirm Password' />
+      <Pressable style={styles.signUpButton} onPress={() => {
+        signUp(email, password);
+      }}>
         <Text>Sign Up</Text>
       </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  signUpContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputContainer: {
+    height: 60,
+    width: '80%',
+    backgroundColor: 'yellow',
+    marginBottom: 5
+  },
+  signUpButton: {
+    height: 60,
+    width: '40%',
+    backgroundColor: 'red',
+  }
+})
 
 export default SignUp;

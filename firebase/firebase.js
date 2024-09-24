906980c16617eb40d2f2fbbd7e4cd0e2caa9270f
Ganesh Patra <40396630+dangenmaster2@users.noreply.github.com>
2024-09-25 00:03:07 +0530

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,20 +3,22 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAyGmyNIwOxPWKmUNnqv8BqmacrBvXK_So",
+  apiKey: `${API_KEY}`,
   authDomain: "ancilife-151f6.firebaseapp.com",
   databaseURL: "https://ancilife-151f6-default-rtdb.firebaseio.com",
   projectId: "ancilife-151f6",
   storageBucket: "ancilife-151f6.appspot.com",
   messagingSenderId: "745131209241",
-  appId: "1:745131209241:web:af37fb8f9f9759b5243088"
+  appId: `${APP_ID}`
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth=getAuth(app);
-const db=getFirestore(app);
+const fireDb=getFirestore(app);
 const storage=getStorage(app);
+const database = getDatabase()
 
-export {app,auth,db,storage, collection, getDocs}
+export {app,auth,fireDb,storage, collection, getDocs, database}
